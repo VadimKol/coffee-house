06cc12d73a333e00d64864c53b95903f933a9ab1
Vadim Kolymbet <3t0n@inbox.ru>
2023-12-01 14:06:21 +0300

    feat: add responsive design to header and enjoy blocks in index page plus many minor changes

---
+++ coffee-house/js/index.js
@@ -0,0 +1,32 @@
+const addClass = (to, which_class) => document.querySelector(to).classList.add(which_class);
+const removeClass = (from, which_class) => document.querySelector(from).classList.remove(which_class);
+function ShowMobileMenu() {
+    removeClass(".nav-btn-mobile-equal", "active-btn");
+    removeClass(".nav-btn-mobile-cross", "disable-btn");
+    removeClass(".nav--mobile", "disable-mobile");
+
+    addClass(".nav-btn-mobile-equal", "disable-btn");
+    addClass(".nav-btn-mobile-cross", "active-btn");
+    addClass(".nav--mobile", "active-mobile");
+}
+function HideMobileMenu() {
+    removeClass(".nav-btn-mobile-equal", "disable-btn");
+    removeClass(".nav-btn-mobile-cross", "active-btn");
+    removeClass(".nav--mobile", "active-mobile");
+
+    addClass(".nav-btn-mobile-equal", "active-btn");
+    addClass(".nav-btn-mobile-cross", "disable-btn");
+    addClass(".nav--mobile", "disable-mobile");
+}
+function HideMobile(){
+    if (window.innerWidth > "835") {
+        removeClass(".nav-btn-mobile-equal", "disable-btn");
+        removeClass(".nav-btn-mobile-cross", "disable-btn");
+        removeClass(".nav-btn-mobile-equal", "active-btn");
+        removeClass(".nav-btn-mobile-cross", "active-btn");
+        removeClass(".nav--mobile", "active-mobile");
+        removeClass(".nav--mobile", "disable-mobile");
+    }
+}
+
+window.onresize = HideMobile;
