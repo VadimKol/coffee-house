1524fb222078f05cdf963005ba525b5f075f24d8
Vadim Kolymbet <3t0n@inbox.ru>
2023-12-08 19:51:32 +0300

    feat: add video as background for enjoy block

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -43,3 +43,28 @@ function HideMobile(){
 }
 
 window.onresize = HideMobile;
+
+
+let video = document.querySelector(".enjoy-video");
+
+if (window.innerWidth > "1920") {
+    video.setAttribute('src', 'video/video (2160p).mp4');
+} else if (window.innerWidth > "1440") {
+    video.setAttribute('src', 'video/video (1440p).mp4');
+} else if (window.innerWidth > "1080") {
+    video.setAttribute('src', 'video/video (1080p).mp4');
+} else if (window.innerWidth > "720") {
+    video.setAttribute('src', 'video/video (720p).mp4');
+} else if (window.innerWidth > "540") {
+    video.setAttribute('src', 'video/video (540p).mp4');
+} else if (window.innerWidth > "360") {
+    video.setAttribute('src', 'video/video (360p).mp4');
+} else {
+    video.setAttribute('src', 'video/video (240p).mp4');
+}
+video.load();
+video.play();
+/*console.log({
+    src: video.getAttribute('src'),
+    type: video.getAttribute('type'),
+  });*/
