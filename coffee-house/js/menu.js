77575888f4f3785232d6dd2dd179c9d51b687a5d
Vadim Kolymbet <3t0n@inbox.ru>
2023-12-12 19:09:47 +0300

    feat: add modals

--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -79,3 +79,186 @@ for(let product of products){
 }
 
 //=====================PARSING-JSON========================
+
+//====================CREATE-MODAL=========================
+export function CreateModal (item, products, element) {
+    let type;
+    for(let product of products){
+        type = item.querySelector(".grid-" + product.category + "-item__type");
+        if (type !== null && type.innerText === product.name) {
+            let src = item.querySelector(".grid-" + product.category + "-item-box__pic").src;
+            let modal_item__pic = document.createElement("img");
+            let modal_item_info = document.createElement("div");
+            modal_item_info.classList.add("modal-item-info");
+            modal_item__pic.classList.add("modal-item__pic");
+            modal_item__pic.src = src.slice(src.indexOf('coffee-house') + 13);//13 это подрезать место картинки, без http и другой инфы
+            modal_item__pic.alt = product.name;
+            modal_item__pic.width = 680;
+            modal_item__pic.height = 680;
+
+            let modal_item_info__type = document.createElement("p");
+            modal_item_info__type.classList.add("modal-item-info__type");
+            modal_item_info__type.append(product.name);
+            modal_item_info.append(modal_item_info__type);
+
+            let modal_item_info__desc = document.createElement("p");
+            modal_item_info__desc.classList.add("modal-item-info__desc");
+            modal_item_info__desc.append(product.description);
+            modal_item_info.append(modal_item_info__desc);
+
+            let modal_item_info__size = document.createElement("p");
+            modal_item_info__size.classList.add("modal-item-info__size");
+            modal_item_info__size.append("Size");
+            modal_item_info.append(modal_item_info__size);
+
+            let modal_item_info_list_s = document.createElement("ul");
+            modal_item_info_list_s.classList.add("modal-item-info-list-s");
+
+            let li = document.createElement("li");
+            let modal_item_info_list_s__btn = document.createElement("button");
+            modal_item_info_list_s__btn.classList.add("modal-item-info-list-s__btn");
+            modal_item_info_list_s__btn.classList.add("modal-item-info-list-s__btn--active");
+            modal_item_info_list_s__btn.id = "S";
+            modal_item_info_list_s__btn.type = "button";
+            let span = document.createElement("span");
+            span.append("S");
+            modal_item_info_list_s__btn.append(span);
+            let hidden_price = document.createElement("p");
+            hidden_price.classList.add("hidden_price");
+            hidden_price.append(product.sizes.s["add-price"]);
+            modal_item_info_list_s__btn.append(hidden_price);
+            modal_item_info_list_s__btn.append(product.sizes.s.size);
+            li.append(modal_item_info_list_s__btn);
+
+            modal_item_info_list_s.append(li);
+
+            li = document.createElement("li");
+            modal_item_info_list_s__btn = document.createElement("button");
+            modal_item_info_list_s__btn.classList.add("modal-item-info-list-s__btn");
+            modal_item_info_list_s__btn.id = "M";
+            modal_item_info_list_s__btn.type = "button";
+            span = document.createElement("span");
+            span.append("M");
+            modal_item_info_list_s__btn.append(span);
+            hidden_price = document.createElement("p");
+            hidden_price.classList.add("hidden_price");
+            hidden_price.append(product.sizes.m["add-price"]);
+            modal_item_info_list_s__btn.append(hidden_price);
+            modal_item_info_list_s__btn.append(product.sizes.m.size);
+            li.append(modal_item_info_list_s__btn);
+
+            modal_item_info_list_s.append(li);
+
+            li = document.createElement("li");
+            modal_item_info_list_s__btn = document.createElement("button");
+            modal_item_info_list_s__btn.classList.add("modal-item-info-list-s__btn");
+            modal_item_info_list_s__btn.id = "L";
+            modal_item_info_list_s__btn.type = "button";
+            span = document.createElement("span");
+            span.append("L");
+            modal_item_info_list_s__btn.append(span);
+            hidden_price = document.createElement("p");
+            hidden_price.classList.add("hidden_price");
+            hidden_price.append(product.sizes.l["add-price"]);
+            modal_item_info_list_s__btn.append(hidden_price);
+            modal_item_info_list_s__btn.append(product.sizes.l.size);
+            li.append(modal_item_info_list_s__btn);
+
+            modal_item_info_list_s.append(li);
+
+            modal_item_info.append(modal_item_info_list_s);
+
+            let modal_item_info__add = document.createElement("p");
+            modal_item_info__add.classList.add("modal-item-info__add");
+            modal_item_info__add.append("Additives");
+            modal_item_info.append(modal_item_info__add);
+            let modal_item_info_list_a = document.createElement("ul");
+            modal_item_info_list_a.classList.add("modal-item-info-list-a");
+
+            li = document.createElement("li");
+            let modal_item_info_list_a__btn = document.createElement("button");
+            modal_item_info_list_a__btn.classList.add("modal-item-info-list-a__btn");
+            modal_item_info_list_a__btn.id = "add-1";
+            modal_item_info_list_a__btn.type = "button";
+            span = document.createElement("span");
+            span.append("1");
+            modal_item_info_list_a__btn.append(span);
+            hidden_price = document.createElement("p");
+            hidden_price.classList.add("hidden_price");
+            hidden_price.append(product.additives[0]["add-price"]);
+            modal_item_info_list_a__btn.append(hidden_price);
+            modal_item_info_list_a__btn.append(product.additives[0].name);
+            li.append(modal_item_info_list_a__btn);
+
+            modal_item_info_list_a.append(li);
+
+            li = document.createElement("li");
+            modal_item_info_list_a__btn = document.createElement("button");
+            modal_item_info_list_a__btn.classList.add("modal-item-info-list-a__btn");
+            modal_item_info_list_a__btn.id = "add-2";
+            modal_item_info_list_a__btn.type = "button";
+            span = document.createElement("span");
+            span.append("2");
+            modal_item_info_list_a__btn.append(span);
+            hidden_price = document.createElement("p");
+            hidden_price.classList.add("hidden_price");
+            hidden_price.append(product.additives[1]["add-price"]);
+            modal_item_info_list_a__btn.append(hidden_price);
+            modal_item_info_list_a__btn.append(product.additives[1].name);
+            li.append(modal_item_info_list_a__btn);
+
+            modal_item_info_list_a.append(li);
+
+            li = document.createElement("li");
+            modal_item_info_list_a__btn = document.createElement("button");
+            modal_item_info_list_a__btn.classList.add("modal-item-info-list-a__btn");
+            modal_item_info_list_a__btn.id = "add-3";
+            modal_item_info_list_a__btn.type = "button";
+            span = document.createElement("span");
+            span.append("3");
+            modal_item_info_list_a__btn.append(span);
+            hidden_price = document.createElement("p");
+            hidden_price.classList.add("hidden_price");
+            hidden_price.append(product.additives[2]["add-price"]);
+            modal_item_info_list_a__btn.append(hidden_price);
+            modal_item_info_list_a__btn.append(product.additives[2].name);
+            li.append(modal_item_info_list_a__btn);
+
+            modal_item_info_list_a.append(li);
+
+            modal_item_info.append(modal_item_info_list_a);
+
+            let modal_item_info_total = document.createElement("p");
+            modal_item_info_total.classList.add("modal-item-info-total");
+            modal_item_info_total.append("Total:");
+            let modal_item_info_total__price = document.createElement("span");
+            modal_item_info_total__price.classList.add("modal-item-info-total__price");
+            modal_item_info_total__price.append('$' + product.price);
+            modal_item_info_total.append(modal_item_info_total__price);
+            modal_item_info.append(modal_item_info_total);
+
+            let modal_item_info_alert = document.createElement("small");
+            modal_item_info_alert.classList.add("modal-item-info-alert");
+            span = document.createElement("span");
+            modal_item_info_alert.append(span);
+            modal_item_info_alert.append("The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.");
+            modal_item_info.append(modal_item_info_alert);
+            let modal_item_info__btn_close = document.createElement("button");
+            modal_item_info__btn_close.classList.add("modal-item-info__btn-close");
+            modal_item_info__btn_close.type = "button";
+            modal_item_info__btn_close.setAttribute('onclick','closeModal()');
+            modal_item_info__btn_close.append("Close");
+            modal_item_info.append(modal_item_info__btn_close);
+
+            let price = document.createElement("p");
+            price.classList.add("current-price");
+            price.append(product.price);
+            price.style.display = 'none';
+
+            element.append(modal_item__pic);
+            element.append(modal_item_info);
+            element.append(price);
+        }
+    }
+}
+//====================CREATE-MODAL=========================
